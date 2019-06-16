c2c71314f631557fca8d0539b76c332f64f4cb22
Alexey Nenastev <a-nenastev@mail.ru>
2019-06-16 20:36:18 +0300

    Initial Commit

---
+++ TypeYourCard/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TypeYourCard
+//
+//  Created by Alexej Nenastev on 16.06.2019.
+//  Copyright © 2019 Alexej Nenastev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
