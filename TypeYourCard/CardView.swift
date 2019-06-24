3596e5c6f4bb24299a3328b211a733ea90110c35
Alexej Nenastev <alexey.ne.bcs@mail.ru>
2019-06-24 16:26:21 +0300

    card

--- TypeYourCard/CardView.swift
+++ TypeYourCard/CardView.swift
@@ -8,15 +8,34 @@
 
 import SwiftUI
 
-struct CardView : View {
+fileprivate extension Animation {
+    private static let rotateDuration:Double = 0.3
+    
+    static var cardHideDelayed: Animation {
+        Animation.basic(duration: 0.001, curve: .linear).delay(rotateDuration/2)
+    }
+    
+    static var rotateCard: Animation {
+        Animation.basic(duration: rotateDuration, curve: .linear)
+    }
+}
+
+fileprivate struct Logo: View {
+    var body: some View {
+        Text("LOGO")
+    }
+}
+
+fileprivate struct FrontCardSide: View {
+    @EnvironmentObject var payCard: PayCard
+    
     var body: some View {
         VStack {
             HStack {
                 Spacer()
-                Text("LOGO")
+                Logo()
             }
-            Spacer()
-            Spacer()
+            Spacer(minLength: 36)
             HStack {
                 Text("4243")
                 Spacer()
@@ -25,9 +44,9 @@ struct CardView : View {
                 Text("4213")
                 Spacer()
                 Text("9876")
-            }
-            .font(.system(size: 28))
-            .foregroundColor(.white)
+                }
+                .font(.system(size: 28))
+                .foregroundColor(.white)
             Spacer()
             HStack {
                 VStack(alignment: .leading) {
@@ -40,23 +59,56 @@ struct CardView : View {
                     Text("VALID THRU").font(.caption)
                     Text("MM/YY")
                 }
-            }.foregroundColor(.white).opacity(0.6)
-            
-            
+                }
+        }.padding(20)
+        .animation(.cardHideDelayed)
+    }
+}
+
+fileprivate struct BackCardSide: View {
+    @EnvironmentObject var payCard: PayCard
+
+    var body: some View {
+        VStack(spacing: 10) {
+            Color.black.frame(height: 60)
+                .padding([.leading, .trailing], -20)
+                .padding(.top, 15)
+            HStack {
+                Color.gray
+                TextField($payCard.cvc, placeholder: Text("XXX")) 
+            }
+            Spacer()
+            HStack {
+                Spacer()
+                Logo()
+            }
+        }.padding(20)
+        .rotation3DEffect(.degrees(-180), axis: (x: 0, y: 1, z: 0))
+        .animation(.cardHideDelayed)
+    }
+}
+
+
+struct CardView : View {
+    @EnvironmentObject var payCard: PayCard
+    @State var isFront = false
+    
+    var body: some View {
+        ZStack {
+            BackCardSide().opacity(isFront ? 0 : 1)
+            FrontCardSide().opacity(isFront ? 1 : 0)
         }
-            .padding(20)
-            
-        //.aspectRatio(2, contentMode: .fill)
-        .frame(width: 340, height: 212, alignment: .center)
         .background(Color(red: 59/255, green: 58/255, blue: 61/255), cornerRadius: 20)
-        
+        .aspectRatio(1.46, contentMode: .fit)
+        .rotation3DEffect(.degrees(isFront ? 0 : -180), axis: (x: 0, y: 1, z: 0))
+        .tapAction { withAnimation(.rotateCard) { self.isFront.toggle() } }
     }
 }
 
 #if DEBUG
 struct CardView_Previews : PreviewProvider {
     static var previews: some View {
-        CardView()
+        CardView().padding(20).environmentObject(PayCard())
     }
 }
 #endif
---
+++ TypeYourCard/PayCard.swift
@@ -0,0 +1,24 @@
+//
+//  PayCard.swift
+//  TypeYourCard
+//
+//  Created by alexej_ne on 24.06.2019.
+//  Copyright © 2019 Alexej Nenastev. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class PayCard: BindableObject {
+    let didChange = PassthroughSubject<PayCard, Never>()
+    
+    var number: String = ""  {  didSet { notify() } }
+    var holderName: String = ""  {  didSet { notify() } }
+    var validThru: String = "" {  didSet { notify() } }
+    var cvc: String = ""  {  didSet { notify() } }
+    
+    private func notify() {
+        didChange.send(self)
+    }
+}
+
